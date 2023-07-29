63d3fab18d3f1d4730e7850dedd7a5b0f127116a
Toxir Otaxonov <136181446+MadinaAbduvaliyeva988@users.noreply.github.com>
2023-07-29 14:30:36 +0500

    CSS 8-dars
    22-guruh o'quvchisi

---
+++ facebook/js/main.js
@@ -0,0 +1,147 @@
+byWrap = document.getElementsByTagName("div") .namedItem("wrap");
+byrasm = document.getElementsByTagName("div") .namedItem("rasm");
+bynavbar = document.getElementsByTagName("div") .namedItem("navbar");
+bynew = document.getElementsByTagName("div") .namedItem("new");
+byikki = document.getElementsByTagName("div") .namedItem("ikki");
+byfirstname = document.getElementsByTagName("div") .namedItem("firstname");
+bylastname = document.getElementsByTagName("div") .namedItem("lastname");
+bybirthday = document.getElementsByTagName("div") .namedItem("birthday");
+bymonth = document.getElementsByTagName("div") .namedItem("month");
+byday1 = document.getElementsByTagName("div") .namedItem("day1");
+byyear = document.getElementsByTagName("div") .namedItem("year");
+bygender = document.getElementsByTagName("div") .namedItem("gender");
+bybir = document.getElementsByTagName("div") .namedItem("bir");
+byfemale = document.getElementsByTagName("div") .namedItem("female");
+byyoq = document.getElementsByTagName("div") .namedItem("yoq");
+byha = document.getElementsByTagName("div") .namedItem("ha");
+bymale = document.getElementsByTagName("div") .namedItem("male");
+byyoq1 = document.getElementsByTagName("div") .namedItem("yoq1");
+byha1 = document.getElementsByTagName("div") .namedItem("ha1");
+bycustom = document.getElementsByTagName("div") .namedItem("custom");
+byyoq2 = document.getElementsByTagName("div") .namedItem("yoq2");
+byha2 = document.getElementsByTagName("div") .namedItem("ha2");
+byoy = document.getElementsByTagName("div") .namedItem("oy");
+byday = document.getElementsByTagName("div") .namedItem("day");
+byname = document.getElementsByTagName("div") .namedItem("name");
+bybutton = document.getElementsByTagName("div") .namedItem("button");
+byon = document.getElementsByTagName("div") .namedItem("on");
+
+byinput = document.getElementsByTagName("input") .namedItem("input");
+bylabel = document.getElementsByTagName("label") .namedItem("label");
+
+byWrap.style.width = "1200px";
+byWrap.style.margin = "0 auto";
+
+bynavbar.style.width = "500px";
+bynavbar.style.margin = "0 auto";
+bynavbar.style.boxShadow = "0 0 10px rgba(80,80,80,0.5)";
+bynavbar.style.padding = "1 rem 1.2 rem";
+bynavbar.style.boxSizing = "border-box";
+
+byinput.style.width = "100%";
+byinput.style.padding = "0.5rem 1.5rem";
+byinput.style.boxSizing = "border-box";
+byinput.style.border = "1ps solid #cccccc";
+byinput.style.borderRadius = "4px";
+byinput.style.display = "inline-block";
+byinput.style.marginBottom = "10px";
+
+bylabel.style.width = "50%";
+bylabel.style.padding = "0.5rem 1.5rem";
+bylabel.style.boxSizing = "border-box";
+bylabel.style.display = "inline-block";
+bylabel.style.textAlign = "rigth";
+bylabel.style.marginBottom = "10px";
+
+byrasm.style.width = "400px";
+byrasm.style.marginLeft = "400px";
+byrasm.style.marginTop = "-50px";
+
+byikki.style.textAlign = "center";
+
+bynew.style.marginTop = "-10px";
+bynew.style.textAlign= "center";
+
+
+
+byfirstname.style.width = "200px";
+byfirstname.style.position = "absolute";
+
+bylastname.style.position = "relative";
+bylastname.style.width = "200px";
+bylastname.style.marginLeft = "262px";
+
+bybirthday.style.marginBottom = "0";
+bybirthday.style.marginTop = "0";
+bybirthday.style.marginLeft = "-25px";
+bybirthday.style.textAlign = "left";
+
+bymonth.style.width = "450px";
+bymonth.style.position = "absolute";
+bymonth.style.marginLeft = "-12px";
+
+byday1.style.width = "450px";
+byday1.style.position = "absolute";
+byday1.style.marginLeft = "160px";
+
+byyear.style.width = "450px";
+byyear.style.position = "absolute";
+byyear.style.marginLeft = "160px";
+
+bygender.style.marginBottom = "0";
+bygender.style.marginTop = "0"
+bygender.style.marginLeft = "-25px";
+bygender.style.textAlign = "left";
+
+bybir.style.width = "500px";
+bybir.style.display = "inline-block";
+
+byfemale.style.width = "130px";
+byfemale.style.height = "35px";
+byfemale.style.border = "1px solid black";
+
+byyoq.style.position = "absolute";
+
+byha.style.position = "relative";
+byha.style.marginTop = "8px";
+byha.style.marginLeft = "55px";
+
+bymale.style.width = "130px";
+bymale.style.height = "35px";
+bymale.style.border = "1px solid black";
+bymale.style.marginLeft = "160px";
+bymale.style.marginTop = "-42px";
+
+byyoq1.style.position = "absolute";
+
+byha1.style.position = "relative";
+byha1.style.marginTop = "8px";
+byha1.style.marginLeft = "20px";
+
+bycustom.style.width = "130px";
+bycustom.style.height = "35px";
+bycustom.style.border = "1px solid black";
+bycustom.style.marginLeft = "325px";
+bycustom.style.marginTop = "-42px";
+
+byyoq2.style.position = "absolute";
+
+byha2.style.position = "relative";
+byha2.style.marginTop = "8px";
+byha2.style.marginLeft = "55px";
+
+byoy.style.marginLeft = "10px";
+byoy.style.width = "30%";
+
+byday.style.width = "30%";
+
+byname.style.width = "30%";
+
+bybutton.style.width = "50%";
+bybutton.style.height = "40px";
+bybutton.style.marginLeft = "100px";
+bybutton.style.backgroundColor = "limegreen";
+
+byon.style.textAlign = "center";
+byon.style.listStyle = "none";
+
